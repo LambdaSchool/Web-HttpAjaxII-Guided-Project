96c14819488040e61513fdb29ff2a3fd390a9dc9
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-12 15:31:34 +0200

    update deps, get crud going on the backend

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const uuid = require('uuid');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let friends = [
+  { id: uuid(), name: 'Anna', age: 24 },
+  { id: uuid(), name: 'Josemaria', age: 26 },
+  { id: uuid(), name: 'Basil', age: 23 },
+  { id: uuid(), name: 'Emily', age: 28 },
+];
+
+function getAllFriends(req, res) {
+  res.json(friends);
+}
+
+function getFriendById(req, res) {
+  res.json(friends.find(friend => friend.id === req.params.id));
+}
+
+function postNewFriend(req, res) {
+  const friend = { id: uuid(), ...req.body };
+  friends.push(friend);
+  res.json(friend);
+}
+
+function deleteFriendById(req, res) {
+  friends = friends.filter(friend => friend.id !== req.params.id);
+  res.json(req.params.id);
+}
+
+function replaceFriendById(req, res) {
+  friends = friends.filter(friend => friend.id !== req.params.id);
+  const friend = { id: req.params.id, ...req.body };
+  friends.push(friend);
+  res.json(friend);
+}
+
+app.get('/api', (req, res) => res.json('mierda'));
+app.get('/api/friends', getAllFriends);
+app.get('/api/friends/:id', getFriendById);
+app.post('/api/friends', postNewFriend);
+app.delete('/api/friends/:id', deleteFriendById);
+app.put('/api/friends/:id', replaceFriendById);
+
+app.listen(3000, () => console.log(
+  'Friends server listening on port 3000!',
+));
